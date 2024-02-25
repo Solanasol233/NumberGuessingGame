b78dbfd7f23a27a14fc94c00b397c61346e9cf86
Solanasol233 <solanasolisss@gmail.com>
2024-02-25 20:48:05 +0100

    main class bug fix

--- Ratespiel.py
+++ Ratespiel.py
@@ -19,11 +19,7 @@ randomNumber02 = random.randint(0, rangeG)
 guesses = 0
 guesses02 = 0
 playCounter = 0
-# playChecker = False  # True => Player 1 | False => Player 2
-Player = 1
-tries = 3
-# Memo = ["Memo:\n"]
-# Memo2 = ["Memo:\n"]
+maxTries: int = 3
 
 
 class PlayerActionDisplayer:
@@ -54,7 +50,7 @@ class PlayerActionDisplayer:
         self.randomNumber: int = self.__generateRandomNumber()
         self.guessesList: list = []
 
-        self.memoryLabel = "Memo:\n"
+        self.memoryLabel.configure(text="Memo:\n")
 
         self.newRandomValueButton.configure(command=self.__generateNewRandomValue)
         self.checkButton.configure(command=self.__check)
@@ -74,7 +70,8 @@ class PlayerActionDisplayer:
     def __generateNewRandomValue(self):
         self.randomNumber = self.__generateRandomNumber()
         self.guessesList.clear()
-        self.guessesCounterLabel.configure(text=str(self.guessesList.count()))
+
+        self.guessesCounterLabel.configure(text=str(len(self.guessesList)))
         self.indicatorLabel.configure(text="your Number")
         self.numberEntry.delete(-1, "end")
         self.progressBar.set(0)
@@ -93,7 +90,7 @@ class PlayerActionDisplayer:
         return random.randint(0, rangeG)
 
     def __check(self) -> None:
-        global tries
+        global maxTries
 
         guessInput: int = int(self.numberEntry.get())
         if self.__guessIsTooLow(guessInput):
@@ -107,11 +104,32 @@ class PlayerActionDisplayer:
             return
 
         self.guessesList.append(guessInput)
-        self.guessesCounterLabel.configure(text=str(self.guessesList.count()))
-        self.progressBar.set(self.guessesList.count() / tries)
+
+        guessesAsString: str = "\n".join(map(str, self.guessesList))
+        self.memoryLabel.configure(text=f"Memo:\n{guessesAsString}")
+
+        self.guessesCounterLabel.configure(text=str(len(self.guessesList)))
+        self.progressBar.set(len(self.guessesList) / (maxTries))
 
         if self.progressBar.get() == 1:
             self.winningLabel.configure(text="You lost!")
+
+        PlayerActionDisplayer.switchBetweenPlayers()
+
+    @staticmethod
+    def switchBetweenPlayers():
+        global playCounter
+        global player1Action
+        global player2Action
+
+        if (playCounter % 2) == 0:
+            player1Action.activate()
+            player2Action.deactivate()
+        else:
+            player1Action.deactivate()
+            player2Action.activate()
+
+        playCounter += 1
 
 
 ##//FUNCTIONS\\##
@@ -122,105 +140,9 @@ def start():
     global player1Action
     global player2Action
 
-    playCheck()
     player1Action.progressBar.set(0)
     player2Action.progressBar.set(0)
-
-    global Player
-    Player = 1
-
-
-# FUNC: switch between Player 1 and 2
-def playCheck():
-    global Player
-    global playCounter
-    global player1Action
-    global player2Action
-
-    if (playCounter % 2) == 0:
-        Player = 1
-        player1Action.activate()
-        player2Action.deactivate()
-    else:
-        Player = 2
-        player1Action.deactivate()
-        player2Action.activate()
-
-    playCounter += 1
-
-
-# FUNC: check function for Button to check if higher or lower
-def mainCheck():
-    if Player == 1:  # Stuff for Player 1
-        try:
-            global guesses  # acces to global variable
-            i = entry1.get()
-            if int(i) < randomNumber:
-                label2.configure(text="Your number is too low!")
-            elif int(i) > randomNumber:
-                label2.configure(text="Your number is too high!")
-            else:
-                label2.configure(text="Congrats! You found the right number!")
-                winLabel.configure(text="You won!")
-            guesses += 1  # Versuchszähler erhöhen
-            Labelguesses.configure(text=str(guesses))
-        except ValueError:
-            Labelguesses.configure(text="Invalid input!")
-        if int(i) > rangeG:
-            Labelguesses.configure(text="Invalid input!")
-        elif int(i) < 0:
-            Labelguesses.configure(text="Invalid input!")
-        progressBar.set(guesses / tries)
-        if progressBar.get() == 1:
-            winLabel.configure(text="You lost!")
-        Memo.append(str(i) + "\n")
-        memoLabel.configure(text="".join(Memo))
-    if Player == 2:  # Stuff for Player 2
-        try:
-            global guesses02  # access to global variable
-            i02 = entry102.get()
-            if int(i02) < randomNumber02:
-                label202.configure(text="Your number is too low!")
-            elif int(i02) > randomNumber02:
-                label202.configure(text="Your number is too high!")
-            else:
-                label202.configure(text="Congrats! You found the right number!")
-                winLabel02.configure(text="You won!")
-            guesses02 += 1  # increase guesses
-            Labelguesses02.configure(text=str(guesses02))
-        except ValueError:
-            Labelguesses02.configure(text="Invalid input!")
-        if int(i02) > rangeG:
-            Labelguesses02.configure(text="Invalid input!")
-        elif int(i02) < 0:
-            Labelguesses02.configure(text="Invalid input!")
-        progressBar02.set(guesses / tries)
-        if progressBar02.get() == 1:
-            winLabel02.configure(text="You lost!")
-        Memo2.append(str(i02) + "\n")
-        memoLabel2.configure(text="".join(Memo2))
-    playCheck()
-
-
-# # FUNC: generate new, independence random value and reset entry1, guesses and guesses label
-# def newRandom():
-#     global randomNumber
-#     global randomNumber02
-#     global player1Action
-#     random.seed()
-#     randomNumber = random.randint(0, rangeG)
-#     randomNumber02 = random.randint(0, rangeG)
-#     global guesses
-#     global guesses02
-#     guesses = 0
-#     guesses02 = 0
-#     Labelguesses.configure(text=str(guesses))
-#     Labelguesses02.configure(text=str(guesses02))
-#     label2.configure(text="your number")
-#     label202.configure(text="your number")
-#     entry1.delete(-1, "end")
-#     entry102.delete(-1, "end")
-#     player1Action.progressBar.set(0)
+    PlayerActionDisplayer.switchBetweenPlayers()
 
 
 ##//GRAPHICAL USER INTERFACE (GUI) PARTS\\##
@@ -239,8 +161,6 @@ root.rowconfigure(0, weight=1)
 root.rowconfigure(1, weight=1)
 
 # create frame, is basicly a colored box with round vertices LOL
-
-
 frame = ctk.CTkFrame(master=root)
 frame.grid(row=1, column=1)
 frame.configure(fg_color="lightgreen")
@@ -297,10 +217,10 @@ button_NewRandom02.pack(padx=10, pady=10)
 winLabel02 = ctk.CTkLabel(master=frame2, text="")
 winLabel02.pack(pady=12, padx=10)
 
-memoLabel = ctk.CTkLabel(master=root, text="".join(Memo))
+memoLabel = ctk.CTkLabel(master=root)
 memoLabel.grid(row=1, column=0, pady=5)
 
-memoLabel2 = ctk.CTkLabel(master=root, text="".join(Memo2))
+memoLabel2 = ctk.CTkLabel(master=root)
 memoLabel2.grid(row=1, column=3)
 
 player1Action = PlayerActionDisplayer(
